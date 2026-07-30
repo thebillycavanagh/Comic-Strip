8a7920053773a27dd9235c644bec0fa2eed437c1
thebillycavanagh <thebillycavanagh@gmail.com>
2026-07-30 15:20:01 -0700

    Export firebaseConfig for external usage

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,4 @@
-const firebaseConfig = {
+export firebaseConfig = {
   apiKey: "AIzaSyBy9fcHVJ-RQVY4cREPi_vfKF6n3NKciYo",
   authDomain: "cmg-comic-strip.firebaseapp.com",
   projectId: "cmg-comic-strip",
